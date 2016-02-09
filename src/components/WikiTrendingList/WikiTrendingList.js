75d8fd94855a2b116b29f12f8312f4553217c0b3
brennanerbz <brennan.erbeznik@gmail.com>
2016-02-09 10:54:13 -0800

    added trending list

--- src/components/WikiTrendingList/WikiTrendingList.js
+++ src/components/WikiTrendingList/WikiTrendingList.js
@@ -4,10 +4,45 @@ export default class WikiForm extends Component {
 	static propTypes = {
 	}
 
+	state = {
+		quizzes: ['Artificial Intelligence', 'Electoral College', 'Donald Trump', 'Y Combinator', 'Elon Musk', 'Word of Mouth', 'Hacker News', 'Election']
+	}
+
 	render() {
+		const { quizzes } = this.state;
 		return (
-			<div>
-				Trending List
+			<div style={{marginTop: '40px'}} className="display_flex flex_vertical flex_center">
+				<h2 style={{fontSize: '24px', fontWeight: '300', color: '#2C3239', marginBottom: '20px'}}>
+					Popular quizzes
+				</h2>
+				<div id="popular_list" className="flex_horizontal">
+					<ul className="list">
+						{
+							quizzes
+							.filter((q, i) => { return  i < 4 })
+							.map((quiz) => {
+								return (
+									<li className="list_item">
+										<a className="link">{quiz}</a>
+									</li>
+								)
+							})
+						}
+					</ul>
+					<ul className="list">
+						{
+							quizzes
+							.filter((q, i) => { return  i > 3 && i < 8 })
+							.map((quiz) => {
+								return (
+									<li className="list_item">
+										<a className="link">{quiz}</a>
+									</li>
+								)
+							})
+						}
+					</ul>
+				</div>
 			</div>
 		);
 	}

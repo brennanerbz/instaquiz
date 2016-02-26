80c38878140d6ac388299fffd02aaebb463f4c5f
brennanerbz <brennan.erbeznik@gmail.com>
2016-02-26 09:11:00 -0800

    updated header with new logo and link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -23,68 +23,17 @@ export default class Header extends Component {
 					position: isNotHomeView ? 'fixed' : '',
 					background: '#fff',
 					width: '100%', 
-					height: isNotHomeView && isMobile ? '' : (isMobile ? '40px' : '48px'), 
-					lineHeight: isMobile ? '40px' : '48px',
 					boxShadow: isNotHomeView ? '0px 1px 1px 0px rgba(203,203,203,0.50)' : '',
-					padding: (isNotHomeView && !isMobile) ? '2em 50px' : '',
 					zIndex: '2'
 				}} 
-				className={'display_flex flex_horizontal ' + (isNotHomeView ? 'flex_center' : '')}>
-				<div 
-				style={{maxWidth: '1000px', width: '100%'}} 
-				className={(isMobile ? 'display_flex ' : '') + (isNotHomeView && !isMobile ? 'flex_center' : '')}>
-					{
-						isMobile && !isNotHomeView
-						&&
-						<div style={{marginLeft: 'auto', marginTop: '10px', marginRight: '20px'}} className="flex_item_align_right">
-							<a onClick={() => this.props.openHowItWorks(!show)} className="link">How It Works</a>
-						</div> 
-					}
-					{
-						isNotHomeView
-						&&
-						<div style={{width: isMobile ? '100%' : '', padding: isMobile ? '0 1em' : ''}} className="flex_horizontal flex_nowrap">
-							
-							<h1 
-								onClick={() => pushState(null, '/')}
-								style={{
-									fontSize: isMobile ? '20px' : '26px', 
-									fontWeight: '600', 
-									color: '#2C3239', 
-									marginBottom: isMobile ? '5px' : '7px',
-									marginTop: '9px!important',
-									cursor: 'pointer',
-								}}
-								className={'fade in flex_item_align_left'}>
-								<span className="inline_block">
-									<img style={{height: isMobile ? '32px' : '40px', marginRight: isMobile ? '0px' : '55px'}} src={blueLogo}/>
-								</span>
-								{
-									!isMobile && isNotHomeView && false
-									&&
-									<span>
-										Quizly
-										<span 
-										style={{
-											fontSize: '14px', 
-											color: '#A8B6C1', 
-											marginLeft: '5px'
-										}} 
-										className="inline_block small_text">
-											BETA
-										</span>
-									</span>
-								}
-							</h1>
-							{
-								isNotHomeView
-								&&
-								<div style={{margin: '5px 0 0 15px', width: isMobile ? '100%' : '50%' }} className="flex_item_align_left">
-									<WikiForm isNotHomeView={isNotHomeView} {...this.props}/>
-								</div>
-							}
-						</div>
-					}
+				className={'display_flex flex_center'}>
+				<div className="flex_horizontal" style={{maxWidth: '1000px', width: '100%', padding: isMobile ? '15px 10px 10px' : '15px 0 10px 0'}}>
+					<img src={blueLogo} style={{height: isMobile ? '45px' : '55px'}}/>
+					<ul style={{lineHeight: isMobile ? '45px' : '55px'}} className="flex_container_right link_list">
+						<li className="link_list_item">
+							<a className="grey link">How It Works</a>
+						</li>
+					</ul>
 				</div>
 			</div>
 		);
--- src/containers/Landing/Landing.js
+++ src/containers/Landing/Landing.js
@@ -6,9 +6,8 @@ import Helmet from 'react-helmet';
 import cookie from 'react-cookie';
 import { pushState } from 'redux-router';
 
-// Components
-import WikiForm from '../../components/WikiForm/WikiForm';
-import WikiTrendingList from '../../components/WikiTrendingList/WikiTrendingList';
+// Children
+import Features from './Features';
 
 @connect(state => ({
 	// the list of top wiki articles
@@ -28,35 +27,41 @@ export default class Landing extends Component {
 		const { location, isMobile } = this.props;
 		const halfiPhone = require('../../../static/images/halfIphone.png');
 		return (
-			<div id={style.landing} style={{height: '88%', borderBottom: '1px solid #8492A6'}} id={style.landing} className="display_flex flex_center">
-				<div style={{marginBottom: '0'}} className="flex_container_center">
-					<div 
-						style={{padding: isMobile ? '0px 20px' : ''}} 
-						className="display_flex flex_vertical flex_center">
-						<h1 
-							style={{
-								fontSize: isMobile ? '28px' : '34px', 
-								fontWeight: '600', 
-								color: '#3C4858', 
-								marginBottom: '20px'
-							}}>
-							Homework. Automated. 
-						</h1>
-						<h2 
-							style={{
-								maxWidth: isMobile ? '350px' : '550px',
-								fontSize: isMobile ? '21px' : '24px',
-								fontWeight: '300',
-								color: '#8492A6',
-								textAlign: 'center'
-							}}>
-							A simple app that automates the way teachers create, assign and grade homework.
-						</h2>
-						<button style={{margin: '20px 0'}} className="button primary_blue">
-							Create assignment
-						</button>
-						<img src={halfiPhone} style={{height: isMobile ? '285px' : '350px'}}/>
+			<div id="landing">
+				<div id="hero">
+					<div id={style.landing} style={{borderBottom: '1px solid #8492A6'}} id={style.landing} className="display_flex flex_center">
+						<div style={{marginBottom: '0', marginTop: '0'}} className="flex_container_center">
+							<div 
+								style={{padding: isMobile ? '0px 20px' : ''}} 
+								className="display_flex flex_vertical flex_center">
+								<h1 
+									style={{
+										fontSize: isMobile ? '28px' : '34px', 
+										fontWeight: '600', 
+										color: '#3C4858', 
+										marginBottom: '20px'
+									}}>
+									Homework. Automated. 
+								</h1>
+								<h2 
+									style={{
+										maxWidth: isMobile ? '350px' : '550px',
+										fontSize: isMobile ? '21px' : '24px',
+										fontWeight: '300',
+										color: '#8492A6',
+										textAlign: 'center'
+									}}>
+									A simple app that automates the way teachers create, assign and grade homework.
+								</h2>
+								<button style={{margin: '20px 0'}} className="button primary_blue">
+									Create assignment
+								</button>
+								<img src={halfiPhone} style={{height: isMobile ? '285px' : '350px'}}/>
+							</div>
+						</div>
 					</div>
+				</div>
+				<div id="features_container">
 				</div>
 			</div>
 		);

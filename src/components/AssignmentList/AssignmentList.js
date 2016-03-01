530a01ffe83398a5a1d8169986ae762b8c050a81
brennanerbz <brennan.erbeznik@gmail.com>
2016-03-01 13:37:05 -0800

    added set icon instead of circle

--- src/components/AssignmentList/AssignmentList.js
+++ src/components/AssignmentList/AssignmentList.js
@@ -16,6 +16,8 @@ export default class AssignmentList extends Component {
 		const assignmentsLength = assignments && assignments.length;
 		const emptyRows = assignmentsLength < 6 ? (6 - assignmentsLength) : 0
 		const forwardArrow = require('../../../static/icons/forwardArrowGrey.png')
+		const setIcon = require('../../../static/icons/set_icon_lines.png')
+
 		// Touch
 		const { touching } = this.state;
 		// Hover
@@ -23,12 +25,12 @@ export default class AssignmentList extends Component {
 		return (
 			<div 
 			className="display_flex flex_vertical flex_container_center relative" 
-			style={{maxWidth: !isMobile ? '1050px' : '', padding: '0 25px'}}>
+			style={{maxWidth: !isMobile ? '1050px' : '', padding: isMobile ? '' : '0 25px'}}>
 				<ul 
 				style={{
 					padding: isMobile ? '3.5em 0 0' : '',
 					margin: isMobile ? '0' : '6em 0',
-					listStyleType: 'none', 
+					listStyleType: 'none',  
 					width: '100%',
 					border: isMobile ? '' : '1px solid #e4e4e4',
 					borderRadius: '4px'
@@ -50,22 +52,8 @@ export default class AssignmentList extends Component {
 							}}
 							key={assignment.title + i} 
 							className="display_flex flex_horizontal flex_nowrap">
-								<span style={{
-											position: 'absolute',
-											display: 'block',
-											height: '35px',
-											width: '35px',
-											lineHeight: '35px',
-											borderRadius: '50%',
-											fontWeight: '500',
-											fontSize: isMobile ? '15px' : '17px', 
-											textAlign: 'center',
-											background: '#1FB6FF',
-											color: '#fff'
-										}}>
-									{assignment.title.charAt(0).toUpperCase()}
-								</span>
-								<span style={{marginLeft: '50px', fontSize: isMobile ? '15px' : '17px'}}>
+								<img src={setIcon} style={{height: '32px', width: '27px'}}/>
+								<span style={{marginLeft: '20px', fontSize: isMobile ? '16px' : '17px'}}>
 									<p style={{fontWeight: '600', color: '#3C4858'}}>{assignment.title}</p>
 									<p style={{fontWeight: '400', color: '#8492A6', lineHeight: '1.25em'}}>
 									{assignment.text.slice(0, 30) + (assignment.text.length > 30 && '...')}
@@ -75,7 +63,7 @@ export default class AssignmentList extends Component {
 								style={{
 									position: 'absolute', 
 									right: isMobile ? '1em' : '3em', 
-									fontSize: isMobile ? '14px' : '16px', 
+									fontSize: isMobile ? '15px' : '16px', 
 									color: '#8492A6'
 								}} className="display_flex flex_horizontal">
 									<p style={{marginRight: '5px'}}>

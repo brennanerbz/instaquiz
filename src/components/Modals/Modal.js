ddbd57f60bf25a190f8a0a12e9f5c7faecdcf127
brennanerbz <brennan.erbeznik@gmail.com>
2016-03-19 17:29:53 -0700

    desktop modal

--- src/components/Modals/Modal.js
+++ src/components/Modals/Modal.js
@@ -98,7 +98,7 @@ export default class DefaultModal extends Component {
 					</div>
 				}
 				<Modal.Body 
-				bsClass={(type === 'phone' ? 'display_flex flex_container_center' : '') + ' '  + 'modal-body'} 
+				bsClass={'modal-body' + ' ' + (type === 'phone' ? 'display_flex flex_container_center' : '')} 
 				style={{
 					padding: bodyPadding,
 					background: isMobile 
